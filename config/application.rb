6fdc7d03b8b66fce377e519562753ad671094ab1
Ysaakue <isaacgondim82@gmail.com>
2020-07-13 23:51:06 -0300

    cors configuration

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,18 @@ module Inventory
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Cors configurations
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          expose: ['access-token','client','uid'],
+          methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+      end
+    end
   end
 end
