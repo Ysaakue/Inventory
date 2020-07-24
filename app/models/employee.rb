793c0fe7a8dfa5d73e84f5ac7f4a61243efc056d
Ysaakue <isaacgondim82@gmail.com>
2020-07-23 21:10:25 -0300

    add counted items and turn orientation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,6 +4,12 @@ class Employee < ApplicationRecord
   validates :name, presence: { message: "Nome não pode ficar em branco" }
   validates :cpf, presence: { message: "CPF não pode ficar em branco" }
 
+  def counted_products(count_id)
+    sql = "select count(*) as products from (inventory_development.results inner join inventory_development.count_products ON count_products.id = results.count_product_id) where results.employee_id = #{id} and count_products.count_id = #{count_id}"
+    result = ActiveRecord::Base.connection.exec_query(sql)
+    result.first["products"]
+  end
+
   def as_json options={}
     {
       id: id,
