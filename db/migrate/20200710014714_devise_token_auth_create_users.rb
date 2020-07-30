3d88293db23fe645aac394c60d766563aa47a6b8
Ysaakue <isaacgondim82@gmail.com>
2020-07-30 17:29:37 -0300

    rename reset password token index unique

--- db/migrate/20200710014714_devise_token_auth_create_users.rb
+++ db/migrate/20200710014714_devise_token_auth_create_users.rb
@@ -42,7 +42,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
 
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :reset_password_token, unique: true, name: "index_unique_password_token"
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
   end
