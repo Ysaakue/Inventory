350780275a0124e1a7c1a12e0c54dbdc8adc1d87
Ysaakue <isaacgondim82@gmail.com>
2020-07-30 17:59:01 -0300

    decrease uid and provider index unique length

--- db/migrate/20200710014714_devise_token_auth_create_users.rb
+++ db/migrate/20200710014714_devise_token_auth_create_users.rb
@@ -41,7 +41,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true, name: "index_uid_provider"
+    add_index :users, [:uid, :provider],     unique: true, name: "index"
     add_index :users, :reset_password_token, unique: true, name: "index_password_token"
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
