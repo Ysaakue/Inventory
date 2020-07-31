23d885edc1c6eb7089914d279c9c61d7a61c18f5
Ysaakue <isaacgondim82@gmail.com>
2020-07-30 23:05:58 -0300

    decrease queries

--- app/models/count_product.rb
+++ app/models/count_product.rb
@@ -5,14 +5,12 @@ class CountProduct < ApplicationRecord
 
   def as_json options={}
     {
-      # id: id,
       product_id: product.id,
       product_code: product.code,
       product_description: product.description,
       product_current_stock: product.current_stock,
       product_unit_measurement: product.unit_measurement,
       product_value: product.value,
-      client: product.client.fantasy_name,
       locations: (!product.location.blank? && !product.location["locations"].blank?)? product.location["locations"] : [],
       quantity_found: results
     }
