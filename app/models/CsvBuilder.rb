96d9d990d5deec37101a72f68cb15934c8de3711
Ysaakue <isaacgondim82@gmail.com>
2020-08-05 23:00:19 -0300

    format values to money

--- app/models/CsvBuilder.rb
+++ app/models/CsvBuilder.rb
@@ -14,8 +14,8 @@ class CsvBuilder
       row << cp.product.code #COD
       row << cp.product.description #MATERIAL
       row << cp.product.unit_measurement #UND
-      row << cp.product.value #VLR UNIT
-      row << cp.product.value * cp.product.current_stock #VLRT TOTAL
+      row << (('%.2f' % cp.product.value).gsub! '.',',') #VLR UNIT
+      row << (('%.2f' % (cp.product.value * cp.product.current_stock)).gsub! '.',',') #VLRT TOTAL
       row << cp.product.current_stock #SALDO INICIAL
       row << (cp.results[0].blank?? '-' : cp.results[0].quantity_found) #CONT 1
       row << (cp.results[1].blank?? '-' : cp.results[1].quantity_found) #CONT 2
@@ -36,7 +36,7 @@ class CsvBuilder
       row << streets.join(',') #RUA
       row << stands.join(',') #ESTANTE
       row << shelfs.join(',') #PRATELEIRA
-      row << cp.results.last.quantity_found * cp.product.value #VLR TOTAL FINAL
+      row << (('%.2f' % (cp.results.last.quantity_found * cp.product.value)).gsub! '.',',') #VLR TOTAL FINAL
       row << ((cp.results.last.quantity_found * cp.product.value)*100)/(cp.product.current_stock * cp.product.value) #RESULTADO VLR %
       output << CSV.generate_line(row)
     end
