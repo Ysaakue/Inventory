e498ecf440834f21dda3a75a1fb69592d1829bae
Ysaakue <isaacgondim82@gmail.com>
2020-09-17 16:28:30 -0300

    permissions to app

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    alias_action :pending_products,:submit_quantity_found, to: :mobile_app
-    can [:read,:update,:mobile_app], :all
-    cannot :create, :all
+    alias_action :pending_products,:submit_quantity_found,:index_by_employee,:identify_employee, to: :mobile_app
+    can :mobile_app, :all
     if user.present?
       can :manage, :all
     end
