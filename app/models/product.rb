a3c5b0214421b4554b7210dd99985123eae027ca
Ysaakue <isaacgondim82@gmail.com>
2020-12-15 23:58:47 -0300

    algorithm to order locations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
   validates :code, uniqueness: { scope: :company, message: "Um produto com esse código já foi cadastrado para essa empresa" }
   validate :can_create, on: :create
 
+  after_update :order_locations
+
   def as_json options={}
     if options   
       if options.key?(:import)
@@ -88,4 +90,64 @@ class Product < ApplicationRecord
       end
     end
   end
+
+  def order_locations
+    if !location.blank? && !location["locations"].blank? && location["locations"].size > 1
+      location["locations"] = quicksort(location["locations"],0,location["locations"].size-1)
+    end
+  end
+
+  private
+  # the functions quicksort and partition reference: https://medium.com/@andrewsouthard1/quicksort-implementation-in-ruby-92de12470efd
+  def quicksort(arr, first, last)
+    if first < last
+      p_index = partition(arr, first, last)
+      quicksort(arr, first, p_index - 1)
+      quicksort(arr, p_index + 1, last)
+    end
+  
+    arr
+  end
+  
+  def partition(arr, first, last)
+    # first select one element from the list, can be any element. 
+    # rearrange the list so all elements less than pivot are left of it, elements greater than pivot are right of it.
+    pivot = arr[last]
+    p_index = first
+    
+    i = first
+    while i < last
+      if  ( !arr[i].blank? && !pivot.blank?) && (
+            !arr[i]["street"].blank? && !pivot["street"].blank? &&
+            arr[i]["street"] < pivot["street"]
+          ) || (
+            !arr[i]["street"].blank? && !pivot["street"].blank? &&
+            !arr[i]["stand"].blank? && !pivot["stand"].blank? &&
+            arr[i]["street"] == pivot["street"] && 
+            arr[i]["stand"] < pivot["stand"]
+          ) || (
+            !arr[i]["street"].blank? && !pivot["street"].blank? &&
+            !arr[i]["stand"].blank? && !pivot["stand"].blank? &&
+            !arr[i]["shelf"].blank? && !pivot["shelf"].blank? &&
+            arr[i]["street"] == pivot["street"] && 
+            arr[i]["stand"] == pivot["stand"] &&
+            arr[i]["shelf"] <= pivot["shelf"] 
+          ) || (
+            !arr[i]["pallet"].blank? && !pivot["pallet"].blank? &&
+            arr[i]["pallet"] <= pivot["pallet"]
+          )
+        temp = arr[i]
+        arr[i] = arr[p_index]
+        arr[p_index] = temp
+        p_index += 1
+      end
+      i += 1
+    end
+    temp = arr[p_index]
+    arr[p_index] = pivot
+    arr[last] = temp
+    return p_index
+  end
+
+  handle_asynchronously :order_locations
 end
