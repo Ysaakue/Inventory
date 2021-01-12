8cb9a3ceec08d3250e79a3f22258204bdff6af2b
Ysaakue <isaacgondim82@gmail.com>
2021-01-12 20:03:42 -0300

    refactoring verify result

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,47 +4,51 @@ class Result < ApplicationRecord
   after_update :verify_result
 
   def verify_result
-    if count_product.results.size == 1 && count_product.results.order(:order)[0].quantity_found != -1
-      Result.new(
-        count_product_id: count_product.id,
-        order: 2,
-      ).save!
-    elsif count_product.results.size == 2 
-      if  count_product.results.order(:order)[0].quantity_found != -1 &&
-          count_product.results.order(:order)[1].quantity_found != -1 &&
+    if count_product.count.first_count? && count_product.results.find_by(order: 1).quantity_found != -1
+      if !count_product.results.find_by(order: 2).present?
+        Result.new(
+          count_product_id: count_product.id,
+          order: 2,
+        ).save!
+      end
+    elsif count_product.count.second_count? &&
+          count_product.results.find_by(order: 1).quantity_found != -1 &&
+          count_product.results.find_by(order: 2).quantity_found != -1 &&
           (
             !count_product.product.location["locations"].blank? &&
             !count_product.product.location["counted_on_step"].blank? &&
             count_product.product.location["counted_on_step"].size == count_product.product.location["locations"].size
           )
-        if  count_product.results.order(:order)[0].quantity_found != count_product.results.order(:order)[1].quantity_found ||
-            count_product.results.order(:order)[0].quantity_found != count_product.product.current_stock ||
-            count_product.results.order(:order)[1].quantity_found != count_product.product.current_stock
+      if  count_product.results.find_by(order: 1).quantity_found != count_product.results.find_by(order: 2).quantity_found ||
+          count_product.results.find_by(order: 1).quantity_found != count_product.product.current_stock ||
+          count_product.results.find_by(order: 2).quantity_found != count_product.product.current_stock
+        if !count_product.results.find_by(order: 3).present?
           Result.new(
             count_product_id: count_product.id,
             order: 3,
           ).save!
-        else # quantity founds are equal
-          count_product.combined_count = true
-          count_product.save(validate: false)
-          count_product.calculate_attributes
         end
-      end # values != -1
-    elsif count_product.results.size == 3 &&
-          count_product.results.order(:order)[2].quantity_found != -1 &&
-          (
-            !count_product.product.location["locations"].blank? &&
-            !count_product.product.location["counted_on_step"].blank? &&
-            count_product.product.location["counted_on_step"].size == count_product.product.location["locations"].size
-          )
-      count_product.combined_count = true
-      count_product.save
-      count_product.calculate_attributes
-    elsif count_product.results.order(:order)[3].quantity_found != -1 &&
-          (
-            !count_product.product.location["locations"].blank? &&
-            !count_product.product.location["counted_on_step"].blank? &&
-            count_product.product.location["counted_on_step"].size == count_product.product.location["locations"].size
+      else # quantity founds are equal
+        count_product.combined_count = true
+        count_product.save(validate: false)
+        count_product.calculate_attributes
+      end
+    elsif (
+            count_product.count.third_count? &&
+            count_product.results.find_by(order: 3).quantity_found != -1 &&
+            (
+              !count_product.product.location["locations"].blank? &&
+              !count_product.product.location["counted_on_step"].blank? &&
+              count_product.product.location["counted_on_step"].size == count_product.product.location["locations"].size
+            )
+          ) || (
+            count_product.count.fourth_count?
+            count_product.results.find_by(order: 4).quantity_found != -1 &&
+            (
+              !count_product.product.location["locations"].blank? &&
+              !count_product.product.location["counted_on_step"].blank? &&
+              count_product.product.location["counted_on_step"].size == count_product.product.location["locations"].size
+            )
           )
       count_product.combined_count = true
       count_product.save!
