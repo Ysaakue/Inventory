725b6d47bbfcfb0d23ea43ed5b0cb60e74d74e20
Ysaakue <isaacgondim82@gmail.com>
2021-04-21 22:26:42 -0300

    fix set attributes

--- app/models/count.rb
+++ app/models/count.rb
@@ -381,8 +381,8 @@ class Count < ApplicationRecord
     final_value_ = 0
     final_stock_ = 0
     counts_products.where(combined_count: true).each do |cp|
-      final_value_ += cp.final_total_value
-      final_stock_ += (cp.results.blank?? 0 : cp.results.order(:order).last.quantity_found)
+      self.final_value_ += cp.final_total_value
+      self.final_stock_ += (cp.results.blank?? 0 : cp.results.order(:order).last.quantity_found)
     end
     self.final_value = final_value_
     self.final_stock = final_stock_
